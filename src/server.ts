fe11b751a1bdda729860f26321b2fe92af85c77f
Bwendel26 <bwendel26999@gmail.com>
2021-02-25 11:29:21 -0300

    Finishing the first class of NLW4; We configure the typescript, and test our initial requisitions get and post in the Insomnia app

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from 'express';
 
 const app = express();
 
+app.get("/", (request, response) => {
+    return response.json({message: "Hello World!"});
+});
+
+app.post("/", (request, response) => {
+    return response.json({message: "The data was successfully saved!"});
+});
+
 app.listen(3333, () => console.log("Server is running..."));
